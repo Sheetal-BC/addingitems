2ebc0c4e47c0c4867c7957af2422205dbd7c0ceb
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-06-25 21:36:45 +0530

    tagadded

--- itemdom.js
+++ itemdom.js
@@ -12,32 +12,51 @@
 // console.log(document.links);
 // console.log(document.images);
 
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// var main = document.querySelector('.title');
-// // console.log(headerTitle);
-// // headerTitle.textContent = ' Hello';
-// // headerTitle.innerText = 'Goodbye'
-// main.style.fontWeight = 'bold';
-// main.style.color = 'green';
-// main.innerHTML = 'ADD ITEMS';
-// header.style.borderBottom = 'solid 3px #000';
+// getelementbyid
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+var main = document.querySelector('.title');
+// console.log(headerTitle);
+// headerTitle.textContent = ' Hello';
+// headerTitle.innerText = 'Goodbye'
+main.style.fontWeight = 'bold';
+main.style.color = 'green';
+main.innerHTML = 'ADD ITEMS';
+header.style.borderBottom = 'solid 3px #000';
+
+// getelementbyclassname
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
 
 
-for(let i = 0; i<items.length; i++){
+// for(let i = 0; i<items.length; i++){
 
-    items[i].style.backgroundColor = 'gray';
-}
-items[2].style.backgroundColor = 'yellow';
+// items[i].style.backgroundColor = 'gray';
+// }
+// items[2].style.backgroundColor = 'yellow';
 
-for(let i = 0; i<items.length; i++){
+// for(let i = 0; i<items.length; i++){
 
-    items[i].style.fontWeight = 'bold';
-}
+//     items[i].style.fontWeight = 'bold';
+// }
+
+
+
+//class getelementbytagname
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+for(let i = 0; i<li.length; i++){
+       li[i].style.backgroundColor = 'gray';
+    }
+    
+
 
 
