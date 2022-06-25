6b8c78e46dadaa0d4954814892a6b25e34fa4c83
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-06-25 21:17:24 +0530

    classnameadded

--- itemdom.js
+++ itemdom.js
@@ -27,7 +27,7 @@ var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+
 
 for(let i = 0; i<items.length; i++){
 
