93726340f41f46e9fd18ca4396b67c5926e11c7e
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-06-25 21:08:15 +0530

    itemsadded

--- itemdom.js
+++ itemdom.js
@@ -12,15 +12,32 @@
 // console.log(document.links);
 // console.log(document.images);
 
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-var main = document.querySelector('.title');
-// console.log(headerTitle);
-// headerTitle.textContent = ' Hello';
-// headerTitle.innerText = 'Goodbye'
-main.style.fontWeight = 'bold';
-main.style.color = 'green';
-main.innerHTML = 'ADD ITEMS';
-header.style.borderBottom = 'solid 3px #000';
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// var main = document.querySelector('.title');
+// // console.log(headerTitle);
+// // headerTitle.textContent = ' Hello';
+// // headerTitle.innerText = 'Goodbye'
+// main.style.fontWeight = 'bold';
+// main.style.color = 'green';
+// main.innerHTML = 'ADD ITEMS';
+// header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+for(let i = 0; i<items.length; i++){
+
+    items[i].style.backgroundColor = 'gray';
+}
+items[2].style.backgroundColor = 'yellow';
+
+for(let i = 0; i<items.length; i++){
+
+    items[i].style.fontWeight = 'bold';
+}
 
 
