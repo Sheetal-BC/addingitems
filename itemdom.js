10c6cd811ad32e9ee77031bffaff862b3f7774a9
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-06-26 14:36:34 +0530

    manupilatingdom

--- itemdom.js
+++ itemdom.js
@@ -100,11 +100,105 @@ title[0].textContent = 'Hello';
 //   even[i].style.backgroundColor = "#ccc";
 // }
 
-let li = document.querySelectorAll('.list-group-item');
-console.log(li);
-li[1].style.color = 'green';
+// let li = document.querySelectorAll('.list-group-item');
+// console.log(li);
+// li[1].style.color = 'green';
 
-let odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0; i<odd.length; i++){
-   odd[i].style.backgroundColor = "green";
-}
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// for(let i=0; i<odd.length; i++){
+//    odd[i].style.backgroundColor = "green";
+// }
+
+
+//MANIPULATING THE DOM
+
+var itemList = document.querySelector('#items');
+
+// parent node
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parent element
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childnode
+// console.log(itemList.childNodes);
+
+// children
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
+
+// firstchild
+// console.log(itemList.firstChild);
+// // firstElementChils
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// lastchild
+console.log(itemList.lastChild);
+// lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+console.log(itemList.nextSibling);
+// nextElementsibing
+console.log(itemList.nextElementSibling);
+
+// previousSibling
+console.log(itemList.previousSibling);
+// previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// create a div
+let newDiv = document.createElement('div');
+
+//  add class
+newDiv.className = 'hello';
+console.log(newDiv);
+
+// add id
+newDiv.id = 'hello 1';
+
+// add attr
+newDiv.setAttribute('title','Hello Div');
+
+// to append before item lister
+
+// create text node
+// let newDivText = document.createTextNode('HELLO WORLD');
+
+// add text to div
+// newDiv.appendChild(newDivText);
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+// container.insertBefore(newDiv,h1);
+
+// to append before item 1
+let newDivt = document.createElement('li');
+newDivt.className = 'list-group-item';
+console.log(newDivt);
+
+let newDivTxt = document.createTextNode('HELLO WORLD');
+newDivt.appendChild(newDivTxt);
+let containr = document.querySelector('#main ul');
+let li = document.querySelector('ul li');
+containr.insertBefore(newDivt,li);
+
+
+
+
+
+
+
+
+
+
+
